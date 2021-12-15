f37f81b298b13ef3a01254a41895c4ec3424e76c
Mahmud <magamedov2010@gmail.com>
2021-12-15 13:43:17 +0300

    Get the form error class

--- src/Presentation/Frontend/Web/Component/Blog/Post/PostController.php
+++ src/Presentation/Frontend/Web/Component/Blog/Post/PostController.php
@@ -57,7 +57,7 @@ final class PostController
                 return $this->webService->getRedirectResponse('blog/index');
             }
 
-            $parameters['errors'] = $form->getFirstErrors();
+            $parameters['errors'] = $form->getFormErrors()->getFirstErrors();
         }
 
         return $this->viewRenderer->render('__form', $parameters);
@@ -97,7 +97,7 @@ final class PostController
             }
 
             $parameters['body'] = $body;
-            $parameters['errors'] = $form->getFirstErrors();
+            $parameters['errors'] = $form->getFormErrors()->getFirstErrors();
         }
 
         return $this->viewRenderer->render('__form', $parameters);

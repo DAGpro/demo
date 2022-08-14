1d8e97c21c3d34283c2796c0db9b1b3f8e0a4d37
Mahmud <magamedov2010@gmail.com>
2022-08-15 02:12:56 +0300

    Fix namespace Backend\SiteController

--- config/routes-backend.php
+++ config/routes-backend.php
@@ -2,19 +2,20 @@
 
 declare(strict_types=1);
 
+use App\Presentation\Backend\Web\Controller\SiteController;
 use Yiisoft\Router\Route;
 
 return [
     \Yiisoft\Router\Group::create('')
         ->routes(
             Route::get('/')
-                ->action([\App\Presentation\Backend\Web\Controller\SiteController::class, 'index'])
+                ->action([SiteController::class, 'index'])
                 ->name('index'),
         )
         ->host('backend.{_host}')
         ->namePrefix('backend/'),
 
     Route::get('/backend')
-        ->action([\App\Backend\Controller\SiteController::class, 'index'])
+        ->action([SiteController::class, 'index'])
         ->name('index'),
 ];
